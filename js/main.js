233d2284069177dfab65705e4310c954e48b3f6f
milooy <milooy@icloud.com>
2015-08-26 13:32:58 +0900

    [week3] change object

--- js/main.js
+++ js/main.js
@@ -6,6 +6,12 @@ var consts = {
     '3': '#list-today'
 };
 
+var Database = {
+    addTODO: function() {
+
+    }
+};
+
 var TODO = {
     init: function() {
         $('.todo-input').keydown(this.add.bind(this));
@@ -13,32 +19,31 @@ var TODO = {
     add: function(e) {
         if($('.todo-input').val() && e.keyCode === 13) {
             var title = $('.todo-input').val();
-            var key = 15;
-            TODO.appendTODOHTML(title, key, 0, 1);
+            var id = new Date().getTime();
+            TODO.appendTODOHTML(title, id, 0, 1);
             $('.todo-input').val(""); //인풋이 많으니 골라 지워
         }
 
     },
-    appendTODOHTML: function(title, id, completed, list) {
-        var parent = $(lists[list])
+    appendTODOHTML: function(title, id, completed, listId) {
+        var parent = $(lists[listId])
         var source   = $("#todo-template").html();
         var template = Handlebars.compile(source);
         var data = {
             id: id,
+            listId: "1",
             title : title,
             completed: completed
         };
         $(parent).append(template(data));
 
-    }
+    },
     /*
     generateElement({
-      id: "123",
-      code: "1", //리스트 이름
-      title: "My Uber Important Task",
-      start_date: "5/9/2015",
-      completed_date: "5/10/2015",
-      completed: '0'
+      id: "1440490127007", //스타트 데이트가 들어감 "5/9/2015",
+      listId: "1", //리스트 이름
+      title: "빨래하기",
+      completed: '0' // 컴플릿 하면 "5/10/2015"처럼 들어감
     });
     */
     remove: function(id) {
